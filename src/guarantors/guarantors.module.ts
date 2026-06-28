35ac4ca3982f505e442c6f1142d4a57862d72dff
Test <wael@gmail.com>
2026-06-28 15:40:50 +0100

    fix guarantors module dependency

--- src/guarantors/guarantors.module.ts
+++ src/guarantors/guarantors.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { GuarantorsController } from './guarantors.controller'
 import { GuarantorsService } from './guarantors.service'
 import { PaymentsModule } from '../payments/payments.module'
+import { KonnectService } from '../payments/konnect.service'
 
 @Module({
   imports: [PaymentsModule],
   controllers: [GuarantorsController],
-  providers: [GuarantorsService],
+  providers: [GuarantorsService, KonnectService],
   exports: [GuarantorsService],
 })
 export class GuarantorsModule {}
