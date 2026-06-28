d7115d2205afb1de9162b0084868d51dffdff4d7
Test <wael@gmail.com>
2026-06-28 16:02:34 +0100

    fix payments module all dependencies

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsController } from './payments.controller'
 import { PaymentsService } from './payments.service'
 import { KonnectService } from './konnect.service'
 import { ScoreModule } from '../score/score.module'
+import { PolicyModule } from '../policy/policy.module'
 
 @Module({
-  imports: [ScoreModule],
+  imports: [ScoreModule, PolicyModule],
   controllers: [PaymentsController],
   providers: [PaymentsService, KonnectService],
   exports: [PaymentsService, KonnectService],
